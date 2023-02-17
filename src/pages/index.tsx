bedc77a0d615264881bde3c059708be96d1985f0
yosuke <yosuke2002jp@yahoo.co.jp>
2023-02-17 15:28:56 +0900

    ナビゲーションバーの実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,20 +8,24 @@ import {
   ListIcon,
   ListItem,
 } from '@chakra-ui/react'
+
 import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
 
 import { Hero } from '../components/Hero'
 import { Container } from '../components/Container'
 import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
 import { CTA } from '../components/CTA'
 import { Footer } from '../components/Footer'
+import {WithSubnavigation} from '../components/WithSubNavigationAndCTA'
 
 import { Flex, Heading, Icon } from "@chakra-ui/react";
 import { SiChakraui } from "react-icons/si";
 import { ChatIcon } from "@chakra-ui/icons";
 
 const Index = () => (
+
+  <>
+  <WithSubnavigation/>
   <Container height="100vh">
     <Hero />
     <Main>
@@ -71,12 +75,12 @@ const Index = () => (
       </List>
     </Main>
 
-    <DarkModeSwitch />
     <Footer>
       <Text>Next ❤️ Chakra</Text>
     </Footer>
     <CTA />
   </Container>
+  </>
 )
 
 export default Index
