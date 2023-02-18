e3ba1116f86da24636d0cdc4f2106c8e79985033
yosuke <yosuke2002jp@yahoo.co.jp>
2023-02-19 03:58:55 +0900

    ウォレット接続を実装するために結局パーツを直書きすることになってしまった。何かやり方があると思うんだけどなぁ。とりあえず、ボタンを押したときの最低限のウォレット接続は出来てる

--- src/components/DesktopSubNav.tsx
+++ src/components/DesktopSubNav.tsx
@@ -26,122 +26,6 @@ import {
 
   import { renderButtunToConnectToWallet } from '../components/HandleButton';
   
-  {/*export const WithSubnavigation= () => {
-    const { isOpen, onToggle } = useDisclosure();
-  
-    return (
-      <Box>
-        <Flex
-          bg={useColorModeValue('#f6a429', 'gray.800')}
-          color={useColorModeValue('gray.600', 'white')}
-          minH={'60px'}
-          py={{ base: 2 }}
-          px={{ base: 4 }}
-          borderBottom={1}
-          borderStyle={'solid'}
-          borderColor={useColorModeValue('gray.200', 'gray.900')}
-          align={'center'}>
-          <Flex
-            flex={{ base: 1, md: 'auto' }}
-            ml={{ base: -2 }}
-            display={{ base: 'flex', md: 'none' }}>
-            <IconButton
-              onClick={onToggle}
-              icon={
-                isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
-              }
-              variant={'ghost'}
-              aria-label={'Toggle Navigation'}
-            />
-            </Flex>
-          <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
-            
-            <Box>
-              <Link
-                p={2}
-                href={'/'}
-                fontSize={'3xl'}
-                fontWeight={700}
-                color={'black'}
-                _hover={{
-                  textDecoration: 'none',
-                  color: 'gray',
-                }}>
-                  NFT MINT SITE
-              </Link>
-            </Box>
-  
-            <Center>
-            <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
-              <DesktopNav />
-            </Flex>
-            </Center>
-          </Flex>
-  
-          <Stack
-            flex={{ base: 1, md: 0 }}
-            justify={'flex-end'}
-            direction={'row'}
-            spacing={6}>
-            {renderButtunToConnectToWallet()}
-          </Stack>
-        </Flex>
-  
-        <Collapse in={isOpen} animateOpacity>
-          <MobileNav />
-        </Collapse>
-      </Box>
-    );
-  }*/}
-  
-  export const DesktopNav = () => {
-    const linkColor = useColorModeValue('gray.600', 'gray.200');
-    const linkHoverColor = useColorModeValue('gray.800', 'white');
-    const popoverContentBgColor = useColorModeValue('white', 'gray.800');
-  
-    return (
-      <Stack direction={'row'} spacing={4}>
-        {NAV_ITEMS.map((navItem) => (
-          
-          <Box key={navItem.label}>
-            <Popover trigger={'hover'} placement={'bottom-start'}>
-              <PopoverTrigger>
-                <Link
-                  p={2}
-                  href={navItem.href ?? '#'}
-                  fontSize={'xl'}
-                  fontWeight={700}
-                  color={'white'}
-                  _hover={{
-                    textDecoration: 'none',
-                    color: linkHoverColor,
-                  }}>
-                  {navItem.label}
-                </Link>
-              </PopoverTrigger>
-  
-              {navItem.children && (
-                <PopoverContent
-                  border={0}
-                  boxShadow={'xl'}
-                  bg={popoverContentBgColor}
-                  p={4}
-                  rounded={'xl'}
-                  minW={'sm'}>
-                  <Stack>
-                    {navItem.children.map((child) => (
-                      <DesktopSubNav key={child.label} {...child} />
-                    ))}
-                  </Stack>
-                </PopoverContent>
-              )}
-            </Popover>
-          </Box>
-        ))}
-      </Stack>
-    );
-  };
-  
   export const DesktopSubNav = ({ label, href, subLabel }: NavItem) => {
     return (
       <Link
@@ -176,7 +60,7 @@ import {
     );
   };
   
-  export const MobileNav = () => {
+  const MobileNav = () => {
     return (
       <Stack
         bg={useColorModeValue('white', 'gray.800')}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,32 @@
 import React, { useEffect, useState, Fragment } from "react";
 
+import {
+  Box,
+  Flex,
+  Text,
+  IconButton,
+  Button,
+  Stack,
+  Collapse,
+  Icon,
+  Link,
+  Center,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  useColorModeValue,
+  useBreakpointValue,
+  useDisclosure,
+} from '@chakra-ui/react';
+import {
+  HamburgerIcon,
+  CloseIcon,
+  ChevronDownIcon,
+  ChevronRightIcon,
+} from '@chakra-ui/icons';
+
 import { Footer } from '../components/Footer'
-import {WithSubnavigation} from '../components/WithSubNavigationAndCTA'
+import { DesktopNav, MobileNav } from '../components/WithSubNavigationAndCTA'
 import { ImgNFT } from '../components/ImageOfNFT'
 import { Describe } from '../components/Description'
 import { TopMessage } from '../components/TopMessage'
@@ -14,11 +39,117 @@ import { CTA } from '../components/CTA'
 
 const Index = () => {
 
+  const { isOpen, onToggle } = useDisclosure();
+
+  const [currentAccount, setCurrentAccount] = useState(null);
+  const [metamaskError, setMetamaskError] = useState(null);
+
+  const handleClick = async () => {
+    const { ethereum } = window as any;    // Buttonクリックで実行 -> クライアントサイドの処理なので、windowが参照できethereumが扱える
+    // ウォレット接続処理
+    if (!ethereum) {
+        alert("Please install Metamask!");
+    }
+    try {
+        const network = await ethereum.request({ method: 'eth_chainId' });
+  
+        if (network.toString() === '0x13881') {
+          const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+          console.log("Found an account! Address: ", accounts[0]);
+          setMetamaskError(null);
+          setCurrentAccount(accounts[0]);
+        }
+  
+        else {
+          setMetamaskError(true);
+        }
+  
+      } catch (err) {
+        console.log(err)
+      }
+}
+
   return(
     <div className="App">
       <header>
         <div className="container-head">
-          <WithSubnavigation/>
+          {/******************ここからトップナビゲーションバー******************/}
+          <Box>
+            <Flex
+              bg={useColorModeValue('#f6a429', 'gray.800')}
+              color={useColorModeValue('gray.600', 'white')}
+              minH={'60px'}
+              py={{ base: 2 }}
+              px={{ base: 4 }}
+              borderBottom={1}
+              borderStyle={'solid'}
+              borderColor={useColorModeValue('gray.200', 'gray.900')}
+              align={'center'}>
+              <Flex
+                flex={{ base: 1, md: 'auto' }}
+                ml={{ base: -2 }}
+                display={{ base: 'flex', md: 'none' }}>
+                <IconButton
+                  onClick={onToggle}
+                  icon={
+                    isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
+                  }
+                  variant={'ghost'}
+                  aria-label={'Toggle Navigation'}
+                />
+                </Flex>
+              <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
+                
+                <Box>
+                  <Link
+                    p={2}
+                    href={'/'}
+                    fontSize={'3xl'}
+                    fontWeight={700}
+                    color={'black'}
+                    _hover={{
+                      textDecoration: 'none',
+                      color: 'gray',
+                    }}>
+                      NFT MINT SITE
+                  </Link>
+                </Box>
+      
+                <Center>
+                <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
+                  <DesktopNav />
+                </Flex>
+                </Center>
+              </Flex>
+      
+              <Stack
+                flex={{ base: 1, md: 0 }}
+                justify={'flex-end'}
+                direction={'row'}
+                spacing={6}>
+                  <Button
+                    as={'a'}
+                    display={{ base: 'none', md: 'inline-flex' }}
+                    onClick={handleClick}
+                    shadow={"md"}
+                    fontSize={'sm'}
+                    fontWeight={600}
+                    color={'white'}
+                    bg={'#F17C1D'}
+                    href={'#'}
+                    _hover={{
+                      bg: '#F9BC30',
+                    }}>
+                    ウォレット接続
+                  </Button>;
+              </Stack>
+            </Flex>
+      
+            <Collapse in={isOpen} animateOpacity>
+              <MobileNav />
+            </Collapse>
+          </Box>
+          {/******************ここまでトップナビゲーションバー*******************/}
         </div>
       </header>
       <div className="top-wrapper">
