ee7b38ba599c0176286a5ce0c843cf483957d9de
yosuke <yosuke2002jp@yahoo.co.jp>
2023-02-22 18:06:52 +0900

    要らないファイルを削除

--- src/pages/vote_manual.tsx
+++ src/pages/vote_manual.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState, Fragment } from "react";
 import { ethers } from 'ethers';
 
 import { Footer } from '../components/Footer'
-import {WithSubnavigation} from '../components/WithSubNavigationAndCTA'
 import { VoteManual } from '../components/VoteManual'
 
 const Index = () => (
@@ -10,7 +9,6 @@ const Index = () => (
   <div className="App">
     <header>
       <div className="container-head">
-        <WithSubnavigation/>
       </div>
     </header>
     <div className="top-wrapper">
